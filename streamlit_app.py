cdea1e700df2e312db0bb6fa898aad364c9e9c4f
jimmily98 <36462181+jimmily98@users.noreply.github.com>
2023-02-07 23:22:37 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,8 +130,8 @@ if confirm:
         df1.to_excel(path)
     st.write('add a line')
     df1 = pd.read_excel(path)
-    st.write([df1.shape[0],user_input,vector])
-    df1.loc[0] = [df1.shape[0],user_input,vector] 
+    st.write([df1.shape[0]+user_input+vector])
+    df1.loc[0] = [df1.shape[0]+user_input+vector] 
     df1.to_excel(path)
         
 
